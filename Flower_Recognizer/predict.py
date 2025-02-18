1666ae1d920ade1313b5e3bf48fb8ea9c0750190
AbdSh17 <abdalraheem200417a@gmail.com>
2025-02-18 21:17:52 +0300

    add source code for python and html

--- Flower_Recognizer/predict.py
+++ Flower_Recognizer/predict.py
@@ -1,16 +1,36 @@
-import pandas as pd
-import numpy as np
-from PIL import Image
-import tensorflow as tf
-import tf_keras
-import argparse
-import tensorflow_hub as hub
-from PIL import Image
-import json
+import os
+import warnings
 
-# Default paths
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+
+warnings.filterwarnings("ignore")
+
+import numpy as np
+
+np.seterr(all="ignore")
+
+import pandas as pd
+
+import tensorflow as tf
+
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+tf.compat.v1.losses.sparse_softmax_cross_entropy = tf.compat.v1.losses.sparse_softmax_cross_entropy
+
+
+import tensorflow_hub as hub
+
+hub.KerasLayer._original_keras_layer_class = None
+
+from PIL import Image
+import argparse
+import json
+import tf_keras
+from flask import Flask, request, jsonify
+
+
 my_image_path = './test_images/hard-leaved_pocket_orchid.jpg'
-my_model = tf_keras.models.load_model("my_model.h5", custom_objects={'KerasLayer': hub.KerasLayer})
+my_model = tf_keras.models.load_model("Flower_Recognizer.h5", custom_objects={'KerasLayer': hub.KerasLayer})
 
 
 def process_image(image):
@@ -22,7 +42,7 @@ def process_image(image):
     image = image.numpy() # re-convert it to numpy array
     return image
 
-def predict(path_image= my_image_path, model=my_model, names_path= "label_map.json", k=5):
+def predict(path_image= my_image_path, model=my_model, names_path= "label_map.json", k=1):
     image = Image.open(path_image) # open the image
     image = np.asarray(image) # convert it to numpy
     image = process_image(image) # pre-process the image
@@ -49,21 +69,43 @@ def predict(path_image= my_image_path, model=my_model, names_path= "label_map.js
 def get_input_args():
     parser = argparse.ArgumentParser(description="Add the four arguments")
 
-    parser.add_argument('image_path', type=str, default='./test_images/hard-leaved_pocket_orchid.jpg', help='path to the images')
-    parser.add_argument('model_path', type=str, default='my_model.h5',help='path to the model')
-    parser.add_argument('--top_k', type=int, default=5, help='number of returned classes along side with their probs')
+    parser.add_argument('--image_path', type=str, default='./test_images/hard-leaved_pocket_orchid.jpg', help='path to the images')
+    parser.add_argument('--model_path', type=str, default='Flower_Recognizer.h5',help='path to the model')
+    parser.add_argument('--top_k', type=int, default=1, help='number of returned classes along side with their probs')
     parser.add_argument('--category_names', type=str, default='label_map.json', help='category_names')
 
     return parser.parse_args()
 
-if __name__ == "__main__":
+app = Flask(__name__)
 
-    in_arg = get_input_args()
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'image' not in request.files:
+        print("No file uploaded")
+        response = jsonify({"message": "No file uploaded"})
+        response.status_code = 400
+        response.headers.add("Access-Control-Allow-Origin", "*")  # Allow all origins
+        return response
 
-    image_path = in_arg.image_path
-    my_model = tf_keras.models.load_model(in_arg.model_path, custom_objects={'KerasLayer': hub.KerasLayer})
-    category_path = in_arg.category_names
-    k = in_arg.top_k
+    file = request.files['image']
+    file_path = f"uploads/{file.filename}"
+    file.save(file_path)  # Save the uploaded file
 
-    probs, classes = predict(image_path, my_model, category_path, k)
+    # in_arg = get_input_args()
+
+    # my_model = tf_keras.models.load_model(my_model, custom_objects={'KerasLayer': hub.KerasLayer})
+    # category_path = in_arg.category_names
+    # k = 1
+
+    probs, classes = predict(path_image= file_path)
+    # probs, classes = predict()
     print(f"Probs: {probs}\nClasses: {classes}")
+
+    print(f"File saved at {file_path}")  # Log file save
+    new_line = '\n'
+    response = jsonify(f"Species: {classes[0]} - Probability: {(probs[0] * 100):.2f}%")
+    response.headers.add("Access-Control-Allow-Origin", "*")  # Allow all origins
+    return response
+
+if __name__ == "__main__":
+    app.run(debug=True)
